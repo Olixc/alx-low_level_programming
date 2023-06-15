4e85eed6d0bbc7ade80b861021fb18fb9aefedc4
Olixc <noibiabdulrasaq@rocketmail.com>
2023-06-15 10:57:43 -0700

    Created: c file to print message without using the stdio.h library and man(3) printf, puts,....

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,21 @@
+/*
+ * File: 101-quote.c
+ * Auth: Noibi O. Abdulrasaq
+ */
+
 #include <unistd.h>
+
 /**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * main - print "and that piece of art is useful" - Dora Korpar, 2015-10-19
- *
- * Return Always 1
- *
+ * Return: Always 1.
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 59);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }

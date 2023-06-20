2f026eac97c85cf919bde7681eec6e716489b84d
Olixc <noibiabdulrasaq@rocketmail.com>
2023-06-20 08:56:55 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * main - prints the alphabets lowercase letter
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}

f9ecb869cb51d99009f6455de2ab4ae1e74e7c63
Olixc <noibiabdulrasaq@rocketmail.com>
2023-06-29 12:40:55 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *cap_string - caplitalize each word in a string
+ * @s: The string value.
+ *
+ * Return: capitalize string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+	const char *delimiters = " \t\n,;.!?\"(){}";
+
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - ('a' - 'A');
+
+	i++;
+
+	while (s[i] != '\0')
+	{
+		if (strchr(delimiters, s[i]) != NULL)
+		{
+			i++;
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - ('a' - 'A');
+			}
+		}
+
+		i++;
+	}
+
+	return (s);
+}

d195ae3bac804e7b3a80fddec043a8e0a5fe5d0c
Olixc <noibiabdulrasaq@rocketmail.com>
2023-06-29 07:39:34 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strcmp - compare two strings
+ * @s1: string value 1
+ * @s2: string value 2
+ *
+ * Return: interger value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int res;
+
+	res = strcmp(s1, s2);
+
+	if (res == 0)
+	{
+		return (res);
+	}
+
+	else if (res < 0)
+	{
+		return (res);
+	}
+	else
+	{
+		return (res);
+	}
+}

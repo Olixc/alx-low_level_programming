1a2fadc1914969aab1f08f6d25358e0d22354aaa
Olixc <noibiabdulrasaq@rocketmail.com>
2023-07-05 06:26:58 -0700

    Task 0

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "main.h"
+/**
+ * _putchar - prints a character to the standard output
+ * @c: the character to be printed
+ *
+ * Return: Always character
+ */
+
+int _putchar(char c)
+{
+	return (putchar(c));
+}

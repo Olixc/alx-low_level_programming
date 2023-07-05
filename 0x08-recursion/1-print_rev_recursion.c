74244dda59dd3a18d60ef46a48e2a37720272d80
Olixc <noibiabdulrasaq@rocketmail.com>
2023-07-05 07:07:38 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse.
+ * @s: String to reverse.
+ *
+ * Return: void.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}

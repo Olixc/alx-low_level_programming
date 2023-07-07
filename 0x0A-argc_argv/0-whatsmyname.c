1b845971b2e8998ef5cb96197b9dd344339caa8c
Olixc <noibiabdulrasaq@rocketmail.com>
2023-07-07 11:47:03 -0700

    fix tax 0 bug

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  *
  * Return: Always zero.
  */
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
-	return 0;	
+	return (0);
 }

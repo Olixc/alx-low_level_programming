f43a1daeb9980437642893c413096851656f924a
Olixc <noibiabdulrasaq@rocketmail.com>
2023-07-07 13:40:11 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two number
+ * @argc: To return failure if arg. is != 2.
+ * @argv: Where to get the two numbers from.
+ *
+ * Return: result of two argv(values). i.e
+ */
+int main(int argc, char *argv[])
+{
+
+	int result = 1;
+	char **ptr = argv + 1;
+
+	if (argc != 3)
+	{
+		fprintf(stderr, "Error\n");
+		return (2);
+	}
+
+	while (*ptr)
+	{
+		result *= atoi(*ptr);
+		ptr++;
+	}
+
+	printf("%d\n", result);
+	return (0);
+}

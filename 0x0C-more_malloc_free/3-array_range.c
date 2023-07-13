4af225ef679afa98de0dbca5824cad249bd2b74f
Olixc <noibiabdulrasaq@rocketmail.com>
2023-07-13 23:47:56 +0000

    hot fix

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,17 +8,16 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	size = max - min + 1;
+	else
+		size = max - min + 1;
 
-	ptr = malloc(sizeof(*ptr) * size);
+	ptr = malloc(sizeof(int) * size);
 
 	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-	{
 		ptr[i] = min + i;
-	}
 
 	return (ptr);
 }

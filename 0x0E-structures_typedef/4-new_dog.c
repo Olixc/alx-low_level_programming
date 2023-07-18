3384d0ee0da1503f9db4bbd8aba647639a45d55b
Olixc <noibiabdulrasaq@rocketmail.com>
2023-07-18 23:46:46 +0000

    Task 4 hot fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,16 +17,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = malloc(strlen(name) + 1);
+	dog->name = malloc(sizeof(char) * strlen(name) + 1);
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(strlen(owner) + 1);
+	dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
 	if (dog->owner == NULL)
 	{
 		free(dog);
+		free(dog->name);
 		return (NULL);
 	}
 	while (name[i++] != '\0')

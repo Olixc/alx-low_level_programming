7ca9be88179d42e6aeaa579da43a49c677f353b4
Olixc <noibiabdulrasaq@rocketmail.com>
2023-07-18 23:51:46 +0000

    Task 4 hot fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,35 +7,37 @@
  * @name: name param
  * @age: age param
  * @owner: owner param
- * Return: new dog pointer
+ *
+ * Return: pointer to new dog, Null otherwise.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int i = 0, j = 0;
 
 	dog = malloc(sizeof(dog_t));
+
 	if (dog == NULL)
 		return (NULL);
+
 	dog->name = malloc(sizeof(char) * strlen(name) + 1);
+
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
+
 	dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
+
 	if (dog->owner == NULL)
 	{
 		free(dog);
 		free(dog->name);
 		return (NULL);
 	}
-	while (name[i++] != '\0')
-		dog->name[i] = name[i];
-	dog->name[i] = '\0';
-	while (owner[j++] != '\0')
-		dog->owner[j] = owner[j];
-	dog->owner[j] = '\0';
+	
+	strcpy(dog->name, name);
+	strcpy(dog->owner, owner);
 	dog->age = age;
 	return (dog);
 }

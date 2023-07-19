6fbe72822f3d4797251b009bfadbc62b59acd432
Olixc <noibiabdulrasaq@rocketmail.com>
2023-07-19 20:50:57 +0000

    Hot fix Task - 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,16 +16,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	
-	if (array == NULL && cmp == NULL)
-		return (-1);
 
-	while (i < size)
+	if (array && cmp)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
-		i++;
-	}
+		while (i < size)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+			i++;
+		}
 
-	return (-1);
+		return (-1);
+	}
+	else
+		return (0);
 }

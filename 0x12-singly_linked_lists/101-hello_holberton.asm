846eace91f7a66fe34ea6d013aac17631eac9ed5
Olixc <noibiabdulrasaq@rocketmail.com>
2023-07-28 20:51:52 +0000

    Task 101 - Hotfix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -4,20 +4,16 @@ extern printf
 section .text
 main:
 	push rbp        ; keep the stack aligned
-	mov rbp, rsp
-
-	; body of code
-	xor eax, eax    ; clear eax (return value for printf)
-	; lea rdi, [msg]  ; store the address of the message into rdi
 	mov rdi, format
 	mov rsi, msg
+	mov rax, 0
 
 	call printf     ; call the printf function
 
-	add rsp, 16
-	leave           ; mov rsp, rbp, pop rbp
+	pop rbp
+	mov rax, 0
 	ret
 
 section .data
-msg db "Hello, Holberton", 0
-format db "%s", 10, 0
+msg: db "Hello, Holberton", 0
+format: db "%s", 10, 0
